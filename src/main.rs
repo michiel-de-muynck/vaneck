968928441f0d2ec995eb4e8ddd96600e39b9211d
Michiel De Muynck <michieldemuynck@gmail.com>
2019-09-28 22:44:09 +0200

    going to remove fast path for search

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,52 @@
+use std::fs::{File, OpenOptions};
+use std::sync::Mutex;
+use std::io::Write;
+use rayon::prelude::*;
+
+#[macro_use]
+extern crate lazy_static;
+
 type N = usize;
 
+lazy_static! {
+    static ref OUTPUT_FILE: Mutex<File> = Mutex::new(
+        OpenOptions::new()
+            .append(true)
+            .create(true)
+            .open("output.txt")
+            .expect("can't open output.txt")
+    );
+    static ref PERIODS_COMPLETED: Mutex<Vec<bool>> = Mutex::new(vec![false; 64]);
+}
+
+fn is_trivial(sequence: &[N]) -> bool {
+    sequence[0] == 1 && sequence[1] == 1
+}
+
+fn output(s: String) {
+    println!("{}", s);
+    let mut output_file = OUTPUT_FILE.lock().expect("can't lock mutex");
+    writeln!(*output_file, "{}", s).expect("failed to write to output file");
+    output_file.sync_data().expect("failed to flush output file");
+}
+
 fn check_sequence(period: N, i: N, prev_num: N, sequence: &[N], last_index: &mut [N]) {
-    /*if i == 0 {
-        println!("Testing sequence: {:?}, period: {}", &sequence[0..period], period);
-    }*/
+    if i == 0 && !is_trivial(sequence) {
+        output(format!("Testing sequence: {:?}, period: {}", &sequence[0..period], period));
+    }
     let prev_num_last_i = last_index[prev_num];
     if prev_num_last_i != 999 {
         last_index[prev_num] = period + i - 1;
         let cur_num = (period + i - 1) - prev_num_last_i;
         if sequence[i] == cur_num {
             if i == period - 3 {
-                println!("Valid sequence found: {:?}, period: {}", &sequence[0..period], period);
+                if is_trivial(sequence) {
+                    output(format!("Trivial sequence found for period: {}", period));
+                } else {
+                    output("==========================================".to_owned());
+                    output(format!("Valid sequence found: {:?}, period: {}", &sequence[0..period], period));
+                    output("==========================================".to_owned());
+                }
             } else {
                 check_sequence(period, i+1, cur_num, sequence, last_index);
             }
@@ -19,51 +55,69 @@ fn check_sequence(period: N, i: N, prev_num: N, sequence: &[N], last_index: &mut
     }
 }
 
-fn extend_sequence(period: N, i: N, prev_num: N, max: N, sequence: &mut [N], last_index: &mut [N]) {
+fn extend_sequence(period: N, i: N, prev_num: N, sequence: &mut [N], last_index: &mut [N], masks: &mut [u64]) {
     debug_assert!(i > 0);
     let prev_num_last_i = last_index[prev_num];
+    if period <= 25 && i >= period-1 && !is_trivial(sequence) {
+        output(format!("Close: {:?}, period: {}, i: {}", &sequence[0..period], period, i));
+    }
     if prev_num_last_i != 999 {
         let cur_num = (i - 1) - prev_num_last_i;
-        if cur_num == period-1 {
-            // period-1 can't appear in the sequence
-            return;
-        }
-        if cur_num > max {
+        let cur_num_mask = 1 << cur_num;
+        if masks[i] & cur_num_mask == 0 {
             return;
         }
         let prev_sequence_i = sequence[i];
-        if prev_sequence_i != 999 && prev_sequence_i != cur_num {
-            return;
-        }
+        debug_assert_eq!(prev_sequence_i, 999);
         sequence[i] = cur_num;
         last_index[prev_num] = i - 1;
+        let prev_mask = std::mem::replace(&mut masks[i], cur_num_mask);
         if i == period - 1 {
             check_sequence(period, 0, cur_num, sequence, last_index);
         } else {
-            extend_sequence(period, i+1, cur_num, max, sequence, last_index);
+            extend_sequence(period, i+1, cur_num, sequence, last_index, masks);
         }
+        masks[i] = prev_mask;
         last_index[prev_num] = prev_num_last_i;
         sequence[i] = prev_sequence_i;
     } else {
         let prev_sequence_i = sequence[i];
-        for n in i..=max {
-            if prev_sequence_i != 999 && prev_sequence_i != n {
+        debug_assert_eq!(prev_sequence_i, 999);
+        for n in i..=period {
+            let n_mask = 1 << n;
+            let prev_num_mask = 1 << prev_num;
+            if masks[i] & n_mask == 0 {
                 continue;
             }
-            if n == period-1 {
-                // period-1 can't appear in the sequence
-                continue;
-            } else if n == period {
+            if n == period {
                 if prev_num == period {
                     // can't have 2x period in a row
                     continue;
                 }
+                let mut backup_mask = 0u64;
+                for mask in masks[i+1..period].iter_mut() {
+                    backup_mask <<= 1;
+                    if *mask & prev_num_mask != 0 {
+                        *mask -= prev_num_mask;
+                    }
+                    debug_assert_ne!(*mask, 0);
+                }
                 last_index[prev_num] = i - 1;
                 sequence[i] = n;
+                let prev_mask = std::mem::replace(&mut masks[i], n_mask);
                 if i == period - 1 {
                     check_sequence(period, 0, n, sequence, last_index);
                 } else {
-                    extend_sequence(period, i+1, n, max, sequence, last_index);
+                    extend_sequence(period, i+1, n, sequence, last_index, masks);
+                }
+                masks[i] = prev_mask;
+                for mask in masks[i+1..period].iter_mut().rev() {
+                    debug_assert_eq!(*mask & prev_num_mask, 0);
+                    if backup_mask & 1 != 0 {
+                        *mask |= prev_num_mask;
+                    }
+                    debug_assert_ne!(*mask, 0);
+                    backup_mask >>= 1;
                 }
                 sequence[i] = prev_sequence_i;
                 last_index[prev_num] = prev_num_last_i;
@@ -75,52 +129,208 @@ fn extend_sequence(period: N, i: N, prev_num: N, max: N, sequence: &mut [N], las
                 dbg!(&sequence[0..period]);*/
                 debug_assert!(projected_n > i);
                 debug_assert!(projected_n < period);
-                if sequence[projected_n] != 999 {
-                    debug_assert_ne!(sequence[projected_n], prev_num);
+                let prev_projected_n_mask = masks[projected_n];
+                if prev_projected_n_mask & prev_num_mask == 0 {
                     continue;
-                } else {
+                }
+                if i < period &&
+                    (
+                        projected_n == period-1 ||
+                        projected_n == i+1 ||
+                        masks[projected_n-1].count_ones() == 1 ||
+                        masks[projected_n+1].count_ones() == 1
+                    )
+                {
+                    let mut masks = masks[0..period].to_owned();
+
+                    for mask in masks[projected_n+1..period].iter_mut() {
+                        if *mask & prev_num_mask != 0 {
+                            *mask -= prev_num_mask;
+                        }
+                    }
+
+                    // pairs of (location, should_be_mask)
+                    let mut test_locs = vec![(projected_n, prev_num_mask)];
+                    let mut impossible = false;
+
+                    while let Some((test_loc, test_target_mask)) = test_locs.pop() {
+                        let prev_mask = masks[test_loc];
+                        masks[test_loc] &= test_target_mask;
+                        if masks[test_loc] == 0 {
+                            impossible = true;
+                            break;
+                        }
+                        if masks[test_loc] == prev_mask {
+                            continue;
+                        }
+
+                        // project test_loc backwards
+                        let next_el_pos = if test_loc == period-1 { 0 } else { test_loc+1 };
+                        let next_el_mask = masks[next_el_pos];
+                        debug_assert!(next_el_mask != 0);
+                        if next_el_mask.count_ones() == 1 {
+                            let d = next_el_mask.trailing_zeros() as usize;
+                            debug_assert!(d <= period);
+                            let new_test_loc = if d <= test_loc {
+                                test_loc - d
+                            } else {
+                                period + test_loc - d
+                            };
+                            test_locs.push((new_test_loc, masks[test_loc]));
+                        }
+                        // if we're writing a concrete element x,
+                        // make the elements between test_loc and the minimum projected location not be x
+                        if masks[test_loc].count_ones() == 1 {
+                            let min_d = next_el_mask.trailing_zeros() as usize;
+                            debug_assert!(min_d <= period);
+                            for j in 1..min_d {
+                                let new_test_loc = if j <= test_loc {
+                                    test_loc - j
+                                } else {
+                                    period + test_loc - j
+                                };
+                                test_locs.push((new_test_loc, !masks[test_loc]));
+                            }
+                        }
+                        // project prev_el backwards
+                        let prev_el_pos = if test_loc == 0 { period-1 } else { test_loc-1 };
+                        let prev_el_mask = masks[prev_el_pos];
+                        if masks[test_loc].count_ones() == 1 {
+                            let d = masks[test_loc].trailing_zeros() as usize;
+                            debug_assert!(d <= period);
+                            let new_test_loc = if d <= prev_el_pos {
+                                prev_el_pos - d
+                            } else {
+                                period + prev_el_pos - d
+                            };
+                            test_locs.push((new_test_loc, prev_el_mask));
+                        }
+                        // if prev_el is a concrete element x,
+                        // make the elements between prev_el and the minimum projected location not be x
+                        if prev_el_mask.count_ones() == 1 {
+                            let min_d = masks[test_loc].trailing_zeros() as usize;
+                            debug_assert!(min_d <= period);
+                            for j in 1..min_d {
+                                let new_test_loc = if j <= prev_el_pos {
+                                    prev_el_pos - j
+                                } else {
+                                    period + prev_el_pos - j
+                                };
+                                test_locs.push((new_test_loc, !prev_el_mask));
+                            }
+                        }
+                    }
+
+                    if impossible {
+                        return;
+                    }
+
                     last_index[prev_num] = i - 1;
                     sequence[i] = n;
-                    sequence[projected_n] = prev_num;
                     if i == period - 1 {
                         check_sequence(period, 0, n, sequence, last_index);
                     } else {
-                        extend_sequence(period, i+1, n, max, sequence, last_index);
+                        extend_sequence(period, i+1, n, sequence, last_index, &mut masks);
                     }
                     sequence[i] = prev_sequence_i;
-                    sequence[projected_n] = 999;
                     last_index[prev_num] = prev_num_last_i;
+                } else {
+                    masks[projected_n] = prev_num_mask;
+                    let mut backup_mask = 0u64;
+                    for mask in masks[projected_n+1..period].iter_mut() {
+                        backup_mask <<= 1;
+                        if *mask & prev_num_mask != 0 {
+                            backup_mask += 1;
+                            *mask -= prev_num_mask;
+                        }
+                        debug_assert_ne!(*mask, 0);
+                    }
+                    last_index[prev_num] = i - 1;
+                    sequence[i] = n;
+                    let prev_mask = std::mem::replace(&mut masks[i], n_mask);
+                    if i == period - 1 {
+                        check_sequence(period, 0, n, sequence, last_index);
+                    } else {
+                        extend_sequence(period, i+1, n, sequence, last_index, masks);
+                    }
+                    masks[i] = prev_mask;
+                    sequence[i] = prev_sequence_i;
+                    last_index[prev_num] = prev_num_last_i;
+                    masks[projected_n] = prev_projected_n_mask;
+                    for mask in masks[projected_n+1..period].iter_mut().rev() {
+                        debug_assert_eq!(*mask & prev_num_mask, 0);
+                        if backup_mask & 1 != 0 {
+                            *mask |= prev_num_mask;
+                        }
+                        debug_assert_ne!(*mask, 0);
+                        backup_mask >>= 1;
+                    }
                 }
             }
         }
     }
 }
 
-fn start_sequence(period: N, sequence: &mut [N], last_index: &mut [N]) {
-    debug_assert!(period >= 2);
+fn start_sequence(period: N, sequence: &mut [N], last_index: &mut [N], masks: &mut [u64]) {
+    debug_assert!(period >= 3);
     for n in 1..=period {
         sequence[0] = n;
+        if n == period-1 {
+            continue;
+        }
         //println!("Start seq: {:?}, last_index: {:?}", &sequence[0..period], &last_index[0..period]);
-        extend_sequence(period, 1, n, n, sequence, last_index);
-        sequence[0] = 999;
+        for mask in masks[0..period].iter_mut() {
+            *mask = (1 << (n+1)) - 2;
+            if n == period {
+                *mask -= 1 << (period-1);
+            }
+        }
+        extend_sequence(period, 1, n, sequence, last_index, masks);
     }
 }
 
 fn main() {
-    let mut sequence = Vec::new();
-    let mut last_index = Vec::new();
+    //std::fs::create_dir_all("output").expect("Can't create output directory");
 
-    sequence.resize(100, 999);
-    last_index.resize(100, 999);
+    let start_period = 5;
 
-    for period in 5..=25 {
-        start_sequence(period, &mut sequence, &mut last_index);
+    // set periods below start_period as completed
+    {
+        let mut periods_completed = PERIODS_COMPLETED.lock().unwrap();
+        for p in 0..start_period {
+            periods_completed[p] = true;
+        }
     }
+
+    (start_period..62).into_iter().par_bridge().for_each(|period| {
+        let mut sequence = Vec::new();
+        let mut last_index = Vec::new();
+        let mut masks = Vec::new();
+
+        sequence.resize(3*period as usize, 999);
+        last_index.resize(3*period as usize, 999);
+        masks.resize(3*period as usize, 0);
+
+        start_sequence(period as N, &mut sequence, &mut last_index, &mut masks);
+
+        {
+            let mut periods_completed = PERIODS_COMPLETED.lock().unwrap();
+            assert_eq!(periods_completed[period], false);
+            periods_completed[period] = true;
+
+            let least_uncompleted_period = periods_completed.iter().enumerate().find(|(_, complete)| !**complete).expect("all periods completed?!").0;
+            output(format!("Completed period: {}, least uncompleted period: {}", period, least_uncompleted_period));
+        }
+    });
 }
 
 // almost work (last one wrong):
 // 6, 1, 7, 5, 7, 2, 6
 // 12, 1, 13, 6, 13, 2, 8, 13, 3, 13, 2, 5, 12
+
+// only first one wrong:
+// 5, 1, 5, 2, 5
+// 6, 3, 2, 6, 3, 3
 
 // almost work (2nd-to last wrong):
 // 4, 4, 1, 8, 4, 3, 6, 8
